8849e7f83822cfc8e7fe554267c921301d86d71b
Larissa L Chechi <154359746+larissachechi@users.noreply.github.com>
2024-01-16 20:09:22 -0300

    Update reajuste.c

--- reajuste.c
+++ reajuste.c
@@ -12,7 +12,7 @@
 #define MAX 10
 typedef struct {
     int codigo;
-    int pendentes;
+    int dependentes;
     float salario;
 } FUNCIONARIO;
 
@@ -27,8 +27,8 @@ int main() {
     scanf("%f", &referencia);
 
     for (i = 0; i < MAX; i++) {
-        printf("Informe o codigo, salario e pendencias do funcionario %d:", i + 1);
-        scanf("%d %f %d", &vetor[i].codigo, &vetor[i].salario, &vetor[i].pendentes);
+        printf("Informe o codigo, salario e dependentes do funcionario %d:", i + 1);
+        scanf("%d %f %d", &vetor[i].codigo, &vetor[i].salario, &vetor[i].dependentes);
     }
     reajuste(vetor, referencia);
 
@@ -43,17 +43,17 @@ void reajuste(FUNCIONARIO vetor[], float referencia) {
     for (i = 0; i < MAX; i++) {
         salario_antigo = vetor[i].salario;  // Salva o salário atual
 
-        if (vetor[i].pendentes <= 2) {
+        if (vetor[i].dependentes <= 2) {
             novo_salario = ((3.0 / 100) * salario_antigo) + salario_antigo;
-        } else if (vetor[i].pendentes > 2 && vetor[i].pendentes <= 5 && vetor[i].salario < referencia) {
+        } else if (vetor[i].dependentes > 2 && vetor[i].dependentes <= 5 && vetor[i].salario < referencia) {
             novo_salario = ((10.0 / 100) * salario_antigo) + salario_antigo;
-        } else if (vetor[i].pendentes > 2 && vetor[i].pendentes < 5 && vetor[i].salario > referencia) {
+        } else if (vetor[i].dependentes > 2 && vetor[i].dependentes < 5 && vetor[i].salario > referencia) {
             novo_salario = ((5.0 / 100) * salario_antigo) + salario_antigo;
-        } else if (vetor[i].pendentes >= 5) {
+        } else if (vetor[i].dependentes >= 5) {
             novo_salario = ((15.0 / 100) * salario_antigo) + salario_antigo;
         }
 
-        printf("Funcionario %d tem %d pendencia e seu antigo salario era: %.2f \n foi reajustado para: %2.3f\n", i + 1, vetor[i].pendentes, salario_antigo, novo_salario);
+        printf("Funcionario %d tem %d dependentes e seu antigo salario era: %.2f \n foi reajustado para: %2.3f\n", i + 1, vetor[i].dependentes, salario_antigo, novo_salario);
 
     }
 }
